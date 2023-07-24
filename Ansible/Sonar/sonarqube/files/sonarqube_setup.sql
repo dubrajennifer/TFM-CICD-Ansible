070cb1c71fa5dababe40b395926e10af747b1856
Jennifer <dubrajennifer@gmail.com>
2023-07-24 23:56:24 +0200

    -

--- Ansible/Sonar/sonarqube/files/sonarqube_setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE sonarqube_db CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'sonarqube_user'@'localhost' IDENTIFIED BY 'sonarqube_bd_pwd';
-GRANT ALL PRIVILEGES ON sonarqube_db.* TO 'sonarqube_user'@'localhost';
-FLUSH PRIVILEGES;
-
